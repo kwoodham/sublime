eac82badb6c0e10b3623483ba49e604c494ffdb9
Kurt Woodham <kwoodham@gmail.com>
2015-09-16 14:31:12 -0400

    working trace_back routine

--- trace_back.py
+++ trace_back.py
@@ -6,43 +6,34 @@ import os
 class TraceBackCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
-        top = top = self.window.project_data()['folders'][0]['path']
+        top = sublime.active_window().project_data()['folders'][0]['path']
         a = self.view.file_name()
-        rel_a = os.path.relpath(a,top)
+        a_path = os.path.dirname(a)
+        a_file = os.path.basename(a)
         hitMatrix = []
-        n = int(0)
         for root, dirs, files in os.walk(top):
             for fileName in files:
-                if fileName.endswith(".md"):
+                if fileName.endswith(".md") and os.path.join(root,fileName) != a: # bypass self
+                    relPath = os.path.relpath(a_path,root)
+                    relPath = relPath.replace('\\', '/')
                     f = open(os.path.join(root, fileName), encoding='ascii', errors='surrogateescape')
+                    fn = int(0)  # Want to reference line number in output
                     for line in f:
-                    if line.find(stringToFind) == (0):  # Only match at start of line
-                        n = n + int(1)
-                        p0 = os.path.relpath(root)
-                        relPath = os.path.relpath(p0, p1)
-                        relPath = relPath.replace('\\', '/')
-                        relFileName = relPath + "/" + fileName
-                        i = [fn, fileName, relFileName, line.rstrip('\n')]
-                        hitMatrix.append(i)
-    # Return a sorted (most recent first) list
-    # see https://wiki.python.org/moin/HowTo/Sorting
-    revHitMatrix = sorted(hitMatrix, key=itemgetter(1, 0), reverse=True)
-    return(revHitMatrix)
-
-        sRaw = sublime.get_clipboard()
-        sParse = sRaw.split('|')
-        anchorFile = self.view.file_name()
-        targetFile = sParse[1]
-        if anchorFile == targetFile:
-            outStr = '[' + sParse[0] + '](#' + sParse[2] + ')'
-        else:
-            p1 = os.path.relpath(os.path.dirname(targetFile))
-            p2 = os.path.relpath(os.path.dirname(anchorFile))
-            f2 = os.path.basename(targetFile)
-            relPath = os.path.relpath(p1, p2)
-            relPath = relPath.replace('\\', '/')
-            outStr = '[' + sParse[0] + ']'
-            outStr = outStr + '(' + relPath + '/' + f2
-            outStr = outStr + '#' + sParse[2] + ')'
-        sel = self.view.sel()
-        self.view.insert(edit, sel[0].b, outStr)
+                        fn = fn + 1
+                        if line.find(relPath + "/" + a_file) > (-1):
+                            relPathBack = os.path.relpath(root,a_path)
+                            relPathBack = relPathBack.replace('\\', '/') + "/" + fileName
+                            i = "- [" + fileName + "](" + relPathBack + ") - line: " + str(fn) + "\n"
+                            hitMatrix.append(i)
+        if len(hitMatrix):
+            sel_a = self.view.find("\n^--- Traceback Links ---", 0).a
+            if sel_a == -1:
+                sel_a = self.view.size()
+            sel_b = self.view.size()
+            self.view.erase(edit, sublime.Region(sel_a, sel_b))
+            outStr = "\n--- Traceback Links ---\n\n"
+            end = sel_a + self.view.insert(edit, sel_a, outStr)
+            outStr = "Found " + str(len(hitMatrix)) + " traceback links:\n\n"
+            end = end + self.view.insert(edit, end, outStr)
+            for a in hitMatrix:
+                end = end + self.view.insert(edit, end, a)
