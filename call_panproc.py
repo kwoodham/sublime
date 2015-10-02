ba52545d58d47de19cc85d2e9d92bf377330ee1c
Kurt Woodham <kwoodham@gmail.com>
2015-10-02 09:39:51 -0400

    Update with --login and process STDOUT and STDERR

--- call_panproc.py
+++ call_panproc.py
@@ -18,10 +18,15 @@ class CallPanprocCommand(sublime_plugin.TextCommand):
         # Prepend shell command if provided in settings (needed under windows, not linux):
         if sh_path:
             cmd_str = [sh_path]
+            cmd_str.append('--login')
             cmd_str.append('-c')
         else:
             cmd_str = []
         cmd_str.append(panproc_path + ' ' + file_in)
 
-        subprocess.Popen(cmd_str, shell='TRUE', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p = subprocess.Popen(cmd_str, shell='TRUE', stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+        if p[0].decode(encoding='UTF-8'):
+            print('STDOUT: ' + p[0].decode(encoding='UTF-8'))
+        if p[1].decode(encoding='UTF-8'):
+            print('STDERR: ' + p[1].decode(encoding='UTF-8'))
         return
