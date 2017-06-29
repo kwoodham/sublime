496195b3a96e78342d5ebb9cef114c90415de889
Kurt Woodham <kwoodham@gmail.com>
2017-06-29 11:42:09 -0400

    Add in fix from rename.py
    Added in regex that would take "---" and convert to "-"

--- slugit.py
+++ slugit.py
@@ -1,9 +1,15 @@
 import sys
 import os
+import re
 from slugify import slugify
 
 fileName, fileExt = os.path.splitext(str(sys.argv[1]))
 fileStr = slugify(fileName, "-") + fileExt
+
+# Strip out the repeated "-" that are formed sometimes by slugify
+regex = r"[-]+"
+subst = "-"
+fileStr = re.sub(regex, subst, fileStr, 0)
 
 sys.stdout.write("Rename " + sys.argv[1] + " to " + fileStr + "? [Y/n]")
 choice = input().lower()
