4b0aa979331ecc8f25c9920ea9a6ae8fa06763f1
Kurt Woodham <kwoodham@gmail.com>
2017-11-07 13:06:10 -0500

    Update Outlook routine to allow alternate link types
    Now can process "[msg1]:
    outlook:00000000AD9D40BD8340CA44A8285C20FE5F08B744322100" links

--- open_in_ol.py
+++ open_in_ol.py
@@ -2,6 +2,15 @@ import sublime_plugin
 import sublime
 import subprocess
 
+# 07 Nov 2017 if the GUID is in a link such as:
+# ...
+# [message][msg1] ...
+# ...
+# [msg1]: outlook:00000000AD9D40BD8340CA44A8285C20FE5F08B744322100
+# ...
+# then there is no closing ")" - in which case I am assuming that the link
+# extends to the end of the string.
+
 class OpenInOlCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
@@ -15,7 +24,10 @@ class OpenInOlCommand(sublime_plugin.TextCommand):
         txt = self.view.substr(self.view.line(sel))
         beg = txt.find("outlook:")
         end = txt.find(")", beg)
-        msg = txt[beg:end]
+        if end == -1:
+            msg = txt[beg:]
+        else:
+            msg = txt[beg:end]
 
         try:
             subprocess.Popen([app, arg, msg])
