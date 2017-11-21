0925785242e5287fafa3b4f4c12b763b8509c40c
Kurt Woodham <kwoodham@gmail.com>
2017-11-21 17:00:38 -0500

    update

--- todo_mod.py
+++ todo_mod.py
@@ -1,33 +1,38 @@
-import sublime_plugin
 import sublime
 import shutil
 import tempfile
 import os
-import re
+import datetime
 
 def todo_mod(task, target, change):
 
-	    settings = sublime.load_settings("TodoInterface.sublime-settings")
-        todo_path = settings.get('todo_path', True)
-        f = open(todo_path, 'r')
-        s = f.read()
-        f.close()
+    todo_path = sublime.load_settings("TodoInterface.sublime-settings").get("todo_path")
+    todo_bak_path = sublime.load_settings("TodoInterface.sublime-settings").get("todo_bak_path")
 
-        fb, abs_path = tempfile.mkstemp()
-        new_file = open(fb, 'w', encoding="utf-8")
 
-        l = s.splitlines()
+    f = open(todo_path, 'r')
+    s = f.read()
+    l = s.splitlines()
+    f.close()
 
-        for line in l:
-        	if line == l:
-				l.replace(target,change)
-			new_file.write(l+"\n")
+    fb, abs_path = tempfile.mkstemp()
+    new_file = open(fb, 'w', encoding="utf-8")
 
-        try:
-            shutil.copy(todo_path, todo_path.replace("txt","bak"))
-        except:
-            print("Error creating backup\n")
+    for line in l:
+        if line == task:
+            line.replace(target,change)
+        new_file.write(line+"\n")
+    new_file.close()
 
-        # if the backup was successful, remove the old todo file, and move the new one to it
-        os.remove(todo_path)
-        shutil.move(abs_path,todo_path)
+    t = datetime.datetime.utcnow()
+    t_str = str(t.year)+str(t.month)+str(t.day)+str(t.hour)+str(t.minute)+str(t.second)
+
+    try:
+        shutil.copy(todo_path, todo_bak_path + t_str + "_todo.bak")
+    except:
+        print("Error creating backup\n")
+
+    # if the backup was successful, remove the old todo file, and move the new one to it
+    os.remove(todo_path)
+    shutil.move(abs_path,todo_path)
+    return 
