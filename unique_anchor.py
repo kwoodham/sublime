2d4dd369908187ceccfc54e81fafcedea711af0c
Kurt Woodham <kwoodham@gmail.com>
2017-12-22 08:42:06 -0500

    Cleanup of unique anchor

--- unique_anchor.py
+++ unique_anchor.py
@@ -1,10 +1,9 @@
-import sublime
 import sublime_plugin
 import random
 import datetime
 
-# generate a unique anchor from a date string and the
-# decimal portion of a random. Use this for linking
+# generate a unique anchor from a date string with an
+# appended random integer. Use this for linking
 # to headers that seek to give slugify a hard time
 
 class UniqueAnchorCommand(sublime_plugin.TextCommand):
@@ -12,6 +11,8 @@ class UniqueAnchorCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		random.seed() # uses current time
 		a = datetime.datetime.today().strftime("%Y%m%d%H%M%S")
+		# get the decimal portion of a 0-1 RN as a string.
 		b = str(random.random()).split(".")[1]
+		# insert the anchor after the cursor location
 		sel = self.view.sel()[0]
 		self.view.insert(edit, sel.a, " {#a" + a + b + "}")
