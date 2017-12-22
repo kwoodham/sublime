cecc17caad2a70506b51c3016c571505c9e67a1d
Kurt Woodham <kwoodham@gmail.com>
2017-12-22 09:43:40 -0500

    Anaconda sweep through these

--- unique_anchor.py
+++ unique_anchor.py
@@ -6,13 +6,14 @@ import datetime
 # appended random integer. Use this for linking
 # to headers that seek to give slugify a hard time
 
+
 class UniqueAnchorCommand(sublime_plugin.TextCommand):
 
-	def run(self, edit):
-		random.seed() # uses current time
-		a = datetime.datetime.today().strftime("%Y%m%d%H%M%S")
-		# get the decimal portion of a 0-1 RN as a string.
-		b = str(random.random()).split(".")[1]
-		# insert the anchor after the cursor location
-		sel = self.view.sel()[0]
-		self.view.insert(edit, sel.a, " {#a" + a + b + "}")
+    def run(self, edit):
+        random.seed()  # uses current time
+        a = datetime.datetime.today().strftime("%Y%m%d%H%M%S")
+        # get the decimal portion of a 0-1 RN as a string.
+        b = str(random.random()).split(".")[1]
+        # insert the anchor after the cursor location
+        sel = self.view.sel()[0]
+        self.view.insert(edit, sel.a, " {#a" + a + b + "}")
