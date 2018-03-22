c3231bf3eb2d2aa2b25ffd7e41f76366bb850695
Kurt Woodham <kwoodham@gmail.com>
2018-03-22 12:03:27 -0400

    Add Date routines
    New - see stack overflow link in file

---
+++ add_date.py
@@ -0,0 +1,14 @@
+import datetime
+import sublime_plugin
+
+# https://stackoverflow.com/questions/11879481/can-i-add-date-time-for-sublime-snippet
+
+
+class AddDateCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.run_command("insert_snippet", {"contents": "%s" % datetime.date.today().strftime("%d %B %Y (%A)")})
+
+
+class AddTimeCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.run_command("insert_snippet", {"contents": "%s" % datetime.datetime.now().strftime("%H:%M")})
