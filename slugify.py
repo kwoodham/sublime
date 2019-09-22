9e15316512458512d43f5a383b3d0add2eac4624
Kurt Woodham <kwoodham@gmail.com>
2019-09-22 15:46:55 -0400

    Update slugify.py
    See comments - now complies with pandoc and MarkdownTOC

--- slugify.py
+++ slugify.py
@@ -14,19 +14,29 @@ import os
 # 2016-03-21 took out the "+" in the last re so that "text - text" would
 # be rendered  "text---text" in agreement with how pandoc generates the
 # html anchors.
+#
+# 2019-09-21 os.path.splitext was spliting at the first occurance of "." so
+# it would leave everything after a "." in the filenane un-slugged and
+# append it back onto what was slugged - thinking the whole string was the
+# extension. Best thing to do would be just not break out the extension
+# for files (I generally use lower-case extensions anyhow, and I can't
+# think of an extension that matters).
+# 
+# Also added "." back into re so that string would match what MarkdownTOC 
+# generates (with "." removed from matching pattern) as well as pandoc.
+
 
 
 def slugify(value, separator):
     """ Slugify a string, to make it URL friendly. """
     value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
-    value = re.sub('[^\w\s-]', '', value.decode('ascii')).strip().lower()
-    # return re.sub('[%s\s]+' % separator, separator, value)
+    value = re.sub('[^\w_ .-]', '', value.decode('ascii')).strip().lower()
     return re.sub('[%s\s]' % separator, separator, value)
 
 if __name__ == '__main__':
-    fileName, fileExt = os.path.splitext(str(sys.argv[1]))
-    fileStr = slugify(fileName, "-") + fileExt
-
+    # 2019-09-21 fileName, fileExt = os.path.splitext(str(sys.argv[1]))
+    # 2019-09-21 fileStr = slugify(fileName, "-") + fileExt
+    fileStr = slugify(str(sys.argv[1]), "-")
     sys.stdout.write("Rename " + sys.argv[1] + " to " + fileStr + "? [Y/n]")
     choice = input().lower()
     if choice == '' or choice == 'y':
